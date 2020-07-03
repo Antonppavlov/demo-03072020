9310c11879a2d8de450d752fabac97df947312a7
Antonpavlov <anton.it.pavlov@gmail.com>
2020-07-03 14:31:51 +0300

    Initial commit

---
+++ src/main/java/ru/appavlov/demo/page/VTBPage.java
@@ -0,0 +1,14 @@
+package ru.appavlov.demo.page;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import ru.at.library.core.cucumber.annotations.Name;
+import ru.at.library.core.cucumber.api.CorePage;
+
+@Name("ВТБ")
+public class VTBPage extends CorePage {
+
+    @Name("ВТБ-Онлайн")
+    @FindBy(css = "[href=\"https://www.vtb.ru/app\"]")
+    public SelenideElement buttonOnlineVtb;
+}
